e54849c3cf54544048eb779c18bb4655ec9b8e3b
AurelienMarchal <marchal.aure@gmail.com>
2021-07-05 14:47:11 +0200

    Training can be done with or without wav2vect

--- train_with_wav2vect copy.py
+++ train_with_wav2vect copy.py
@@ -43,8 +43,7 @@ class Speech2CommandBrain(sb.Brain):
             command_bos = torch.cat([command_bos] * self.n_augment)
 
         
-        feats = self.modules.compute_features(wavs)
-        feats = self.modules.normalize(feats)
+        feats = self.modules.wav2vec2(wavs)
 
         encoder_out = self.hparams.enc(feats)
 
@@ -155,21 +154,6 @@ class Speech2CommandBrain(sb.Brain):
                 {"Epoch loaded": self.hparams.epoch_counter.current},
                 test_stats=stage_stats,
             )
-
-    def init_optimizers(self):
-        "Initializes the wav2vec2 optimizer and model optimizer"
-        self.wav2vec_optimizer = self.hparams.wav2vec_opt_class(
-            self.modules.wav2vec2.parameters()
-        )
-        self.optimizer = self.hparams.adam_opt_class(
-            self.hparams.model.parameters()
-        )
-
-        if self.checkpointer is not None:
-            self.checkpointer.add_recoverable(
-                "wav2vec_opt", self.wav2vec_optimizer
-            )
-            self.checkpointer.add_recoverable("opt", self.optimizer)
 
 def dataio_prep(hparams):
     """This function prepares the datasets to be used in the brain class.
