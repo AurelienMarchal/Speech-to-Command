540735344bd42a52dc23c979469fe836974f878b
AurelienMarchal <marchal.aure@gmail.com>
2021-07-05 14:52:47 +0200

    removed init_optimizer

--- train.py
+++ train.py
@@ -156,20 +156,6 @@ class Speech2CommandBrain(sb.Brain):
                 test_stats=stage_stats,
             )
 
-    def init_optimizers(self):
-        "Initializes the wav2vec2 optimizer and model optimizer"
-        self.wav2vec_optimizer = self.hparams.wav2vec_opt_class(
-            self.modules.wav2vec2.parameters()
-        )
-        self.optimizer = self.hparams.adam_opt_class(
-            self.hparams.model.parameters()
-        )
-
-        if self.checkpointer is not None:
-            self.checkpointer.add_recoverable(
-                "wav2vec_opt", self.wav2vec_optimizer
-            )
-            self.checkpointer.add_recoverable("opt", self.optimizer)
 
 def dataio_prep(hparams):
     """This function prepares the datasets to be used in the brain class.
